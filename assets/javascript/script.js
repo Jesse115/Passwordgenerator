77f9348fd6ae13d12b06ecf7576e2adc17dc8456
Jesse <ftsjess@gmail.com>
2022-06-27 22:17:59 -0400

    my 1st commit

---
+++ assets/javascript/script.js
@@ -0,0 +1,10 @@
+var generateBtn = document.querySelector("#generate");
+var specialChar = "!@#$%^&*()_-+={}[];:'`~<,>.?/|"
+var numberChar = "0123456789";
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+var passwordLength;
+var uppercaseCheck;
+var lowercaseCheck;
+var numberCheck;
+var specialCheck;
